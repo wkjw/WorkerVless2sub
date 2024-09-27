d724fd57035d2a09351abb9aabbd04164d1a12cb
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-09-27 14:35:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -16,7 +16,7 @@ let addressesapi = [
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
-let addressesnotls = [
+#let addressesnotls = [
 	'www.visa.com.sg#官方优选域名',
 	'www.wto.org:8080#官方优选域名',
 	'www.who.int:8880#官方优选域名',
