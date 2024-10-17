c459edcd701416d83b678278506e4afc3940ac03
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-10-18 00:37:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -656,7 +656,7 @@ export default {
 
 					return trojanLink;
 				} else {
-					const vlessLink = `vless://${uuid}@${address}:${port}?encryption=none&security=tls&sni=${sni}&alpn=h3&fp=random&type=${type}&host=${伪装域名}&path=${encodeURIComponent(最终路径)}#${encodeURIComponent(addressid + 节点备注)}`;
+					const vlessLink = `vless://${uuid}@${address}:${port}?encryption=none&security=tls&sni=${sni}&type=${type}&host=${伪装域名}&path=${encodeURIComponent(最终路径)}#${encodeURIComponent(addressid + 节点备注)}`;
 			
 					return vlessLink;
 				}
